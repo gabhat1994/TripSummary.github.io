c8fd223b6b55dc1c1e884c8c4e8485ff6403547d
stamic1 <bhatgaurav96@gmail.com>
2020-09-19 11:21:31 +0530

    creating child components

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from "./Header"
+import Collapsable from "./Collapsable"
 
 export default function HomePage() {
     return (
         <div>
-            
+            <Header />
+            <Collapsable />
         </div>
     )
 }
