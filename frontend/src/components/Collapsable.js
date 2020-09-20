bb8d852575eed5bfa56f138cd412c66a819b7da7
stamic1 <bhatgaurav96@gmail.com>
2020-09-20 10:47:41 +0530

    Hide and show expansion section of collapse able

--- frontend/src/components/Collapsable.js
+++ frontend/src/components/Collapsable.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Collapsable() {
-    return (
-        <div>
-            
+  const [show, setShow] = useState(false);
+  const handleToggle = () => {
+    if (show == false) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+  return (
+    <div>
+      <div className="collapse">
+        <div className="collapse-header">
+          <div
+            className="collapse-header-date"
+            style={{ display: "inline-block" }}
+          >
+            Date :
+          </div>
+          <div
+            className="collapse-header-total"
+            style={{ display: "inline-block" }}
+          >Total KM </div>
+          <div
+            className="collapse-header-button"
+            style={{ display: "inline-block" }}
+          >
+            <button onClick={handleToggle}></button>
+          </div>
         </div>
-    )
+        {show && (
+          <div
+            className="collapse-expansion"
+            style={{ border: "1px solid red" }}
+          >
+            <div
+              className="collapse-table"
+              style={{ border: "1px solid red" }}
+            ></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
